64de46e9b2fa10cf658daedc01ec69eff95d7c1f
Rachael Hall <rachael.hall.889@gmail.com>
2018-09-30 16:22:24 +0100

    moved some code and broke the app whoops, fixed it

--- routers/api-router.js
+++ routers/api-router.js
@@ -4,7 +4,7 @@ const articleRouter = require("./article-router");
 const userRouter = require("./user-router");
 const commentRouter = require("./comment-router");
 
-app.use("/", (req, res, next) =>
+apiRouter.use("/", (req, res, next) =>
   res.sendFile(`${__dirname}/views/home-page.html`)
 );
 apiRouter.use("/topics", topicRouter);
